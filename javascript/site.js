466e2240fb7daf1993c6bc4b4a126972f0ad332b
MrFrolin <112981615+MrFrolin@users.noreply.github.com>
2024-02-09 13:59:55 +0100

    update

--- javascript/site.js
+++
@@ -1,7 +0,0 @@
-function initMap() {
-        var mapOptions = {
-            center: { lat: 59.3293, lng: 18.0686 }, // Koordinater för centrum av kartan
-            zoom: 10 // Zoomnivå (ju högre värde, desto närmare zoomad)
-        };
-        var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-    }
