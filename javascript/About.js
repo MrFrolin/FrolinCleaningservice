98c8127c40a736f063cdc909fa95a9c4594de6e6
Philip Frölin <Philip_frolin@hotmail.com>
2024-02-13 20:12:01 +0100

    update

--- javascript/About.js
+++ javascript/About.js
@@ -1,6 +1,6 @@
 async function getWeather(){
 
-    const weatherUrl = "https://api.open-meteo.com/v1/forecast?latitude=58.3912&longitude=13.8451&current=temperature_2m&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min&timezone=auto";
+    const weatherUrl = "https://api.open-meteo.com/v1/forecast?latitude=58.4667&longitude=14.3&current=temperature_2m&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min&timezone=auto";
     const response = await fetch(weatherUrl);
     const results = await response.json();
 
